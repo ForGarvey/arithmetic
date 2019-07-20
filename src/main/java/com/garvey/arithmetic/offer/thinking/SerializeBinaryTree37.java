5b1ffacee48b63bbdc0f0d5e6eee22c8b701828c
Garvey <jwcherish@foxmail.com>
2019-07-20 20:57:46 +0800

    序列化二叉树

---
+++ src/main/java/com/garvey/arithmetic/offer/thinking/SerializeBinaryTree37.java
@@ -0,0 +1,10 @@
+package com.garvey.arithmetic.offer.thinking;
+
+/**
+ * @Description 37
+ * 序列化二叉树：
+ *  请实现两个函数，分别用来序列化和反序列化二叉树。
+ * @Date 2019/7/20 20:55
+ */
+public class SerializeBinaryTree37 {
+}
