2712377383fb2bab5f530b44afc62fa611878e6d
Garvey <jwcherish@foxmail.com>
2019-08-06 14:32:34 +0800

    和为s的两个数字

--- src/main/java/com/garvey/arithmetic/offer/ability/FindNumbersWithSum57.java
+++ src/main/java/com/garvey/arithmetic/offer/ability/FindNumbersWithSum57.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * @Description 57
+ * @Description 57.1
  *  和为s的数字：和为s的两个数字
  *      输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
  *      1.时间复杂度为 O(n²)：
