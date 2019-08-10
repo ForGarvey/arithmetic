981a90386c27b81d3d172919d58166e796ea913b
Garvey <jwcherish@foxmail.com>
2019-08-10 21:43:54 +0800

    扑克牌顺子

--- src/main/java/com/garvey/arithmetic/offer/ability/IsContinuous61.java
+++ src/main/java/com/garvey/arithmetic/offer/ability/IsContinuous61.java
@@ -1,5 +1,7 @@
 package com.garvey.arithmetic.offer.ability;
 
+import java.util.Arrays;
+
 /**
  * @Description 61
  *  扑克牌顺子：
@@ -8,4 +10,27 @@ package com.garvey.arithmetic.offer.ability;
  * @Date 2019-08-09 19:55
  */
 public class IsContinuous61 {
+
+    public boolean isContinuous(int[] nums) {
+
+        if (nums.length < 5)
+            return false;
+
+        Arrays.sort(nums);
+
+        // 统计癞子数量
+        int cnt = 0;
+        for (int num : nums)
+            if (num == 0)
+                cnt++;
+
+        // 使用癞子去补全不连续的顺子
+        for (int i = cnt; i < nums.length - 1; i++) {
+            if (nums[i + 1] == nums[i])
+                return false;
+            cnt -= nums[i + 1] - nums[i] - 1;
+        }
+
+        return cnt >= 0;
+    }
 }
