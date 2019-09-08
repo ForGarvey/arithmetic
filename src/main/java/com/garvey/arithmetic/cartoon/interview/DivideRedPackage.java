04fe0a289acac8518c3b856c0ec259f2d912a819
Garvey <jwcherish@foxmail.com>
2019-09-08 23:46:29 +0800

    拆分红包

---
+++ src/main/java/com/garvey/arithmetic/cartoon/interview/DivideRedPackage.java
@@ -0,0 +1,11 @@
+package com.garvey.arithmetic.cartoon.interview;
+
+/**
+ * @Description 拆分红包
+ *      1.二倍均值法
+ *      2.线段切割法
+ * @Author Garvey
+ * @Date 2019-09-08 23:45
+ */
+public class DivideRedPackage {
+}
