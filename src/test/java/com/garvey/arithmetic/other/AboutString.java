1715e45fb30e3eea884a2f2516ee973b98c04167
Garvey <jwcherish@foxmail.com>
2019-09-10 23:16:17 +0800

    关于拼接字符串

--- src/test/java/com/garvey/arithmetic/other/AboutString.java
+++ src/test/java/com/garvey/arithmetic/other/AboutString.java
@@ -11,7 +11,7 @@ public class AboutString {
 
     @Test
     public void aboutIntern1() {
-        String str2 = new String("str")+new String("01");
+        String str2 = new String("str") + new String("01");
 
         str2.intern();
 
@@ -22,12 +22,26 @@ public class AboutString {
 
     @Test
     public void aboutIntern2() {
-        String str2 = new String("str")+new String("01");
+        String str2 = new String("str") + new String("01");
 
         String str1 = "str01";
 
         str2.intern();
 
         System.out.println(str2 == str1);
+    }
+
+    @Test
+    public void aboutStringAdd1() {
+        String s= "abc";
+        String s1 = new String("abc");
+        String s2 = "a" + "b" + "c";
+        String s3 = "a" + new String("b") + "c";
+        System.out.println(s == s1);
+        System.out.println(s == s2);
+        System.out.println(s == s3);
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s2 == s3);
     }
 }
