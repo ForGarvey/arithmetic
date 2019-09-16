478b23ef85e2b63500fdde958909b8e23775cdc8
Garvey <jwcherish@foxmail.com>
2019-09-16 23:57:41 +0800

    算法题

--- src/test/java/com/garvey/arithmetic/interview/SmartSteal.java
+++ src/test/java/com/garvey/arithmetic/interview/SmartSteal.java
@@ -36,12 +36,23 @@ public class SmartSteal {
             return 0;
         }
 
-        int[] dp = new int[nums.length];
+        if (nums.length == 1) {
+            return nums[0];
+        }
 
-        for (int i = 0; i < nums.length; i++) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        if (nums[0] > nums[1] ||  (nums[0] + nums[2] > dp[1])) {
 
         }
-        return 0;
+
+        return dp[nums.length - 1];
     }
 
     public static void main(String[] args) {
