f0e3f11e2e00945c5803fe10de32eca61c4cffe7
Garvey <jwcherish@foxmail.com>
2019-10-05 23:06:03 +0800

    检查替换后的词是否有效

---
+++ src/main/java/com/garvey/arithmetic/leetcode/medium/IsValid.java
@@ -0,0 +1,13 @@
+package com.garvey.arithmetic.leetcode.medium;
+
+/**
+ * @Description 检查替换后的词是否有效
+ * @Author Garvey
+ * @Date 2019-10-05 23:05
+ */
+public class IsValid {
+
+    public boolean isValid(String S) {
+        return false;
+    }
+}
