33c1281b2ec1621a0c700d03ffb5e149de366ec2
Garvey <jwcherish@foxmail.com>
2019-12-04 18:07:36 +0800

    AliyunExam

---
+++ src/test/java/com/garvey/arithmetic/other/aliyun/exam/java/senior/NumRound.java
@@ -0,0 +1,17 @@
+package com.garvey.arithmetic.other.aliyun.exam.java.senior;
+
+import org.junit.Test;
+
+/**
+ * @Description
+ * @Author Garvey
+ * @Date 2019/12/4 18:06
+ */
+public class NumRound {
+
+    @Test
+    public void numRound() {
+        // -16
+        System.out.println(Math.round(-15.61));
+    }
+}
