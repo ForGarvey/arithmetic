061d0bbb9e6f5cd4b419d20344242f44989bd7ba
Garvey <jwcherish@foxmail.com>
2019-12-19 17:10:21 +0800

    打印char数组

---
+++ src/test/java/com/garvey/arithmetic/other/SoutCharArr.java
@@ -0,0 +1,17 @@
+package com.garvey.arithmetic.other;
+
+import org.junit.Test;
+
+/**
+ * @Description
+ * @Author Garvey
+ * @Date 2019/12/19 17:09
+ */
+public class SoutCharArr {
+
+    @Test
+    public void soutCharArr() {
+        // 结果为abc
+        System.out.print(new char[]{'a','b','c'});
+    }
+}
